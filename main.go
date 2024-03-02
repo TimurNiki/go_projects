843c4e7c6dd461ed411e6c71793ccb213d0fa2cd
TimurNiki <timurniki95@gmail.com>
2024-03-02 12:10:47 +0400

    get and post

--- main.go
+++ main.go
@@ -9,15 +9,24 @@ type Student struct {
 	Teacher string `json:"teacher"`
 }
 
+var students= []Student{
+{ID:1,Name: "John",Class: 10,Teacher: "Mr. X"},
+{ID:2,Name: "Jane",Class: 11,Teacher: "Mr. Y"},
+{ID:3,Name: "Joe",Class: 12,Teacher: "Mr. Z"},
+}
+
 func listOfStudents(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "hello",
-	})
+	c.JSON(200, students)
+}
+
+func createStudent(c *gin.Context){
+
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/students", listOfStudents)
+	router.POST("/students", createStudent)
 	router.Run("localhost:9595")
 
 }
