ad02f19653a03fb6f27390820fd5bb63617e8242
TimurNiki <timurniki95@gmail.com>
2024-03-24 15:25:04 +0400

    auth added

---
+++ auth.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func withJWTAuth(handlerFunc http.HandlerFunc, store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tokenString := GetTokenFromRequest(r)
+		token, err := validateJWT(tokenString)
+
+		if err != nil {
+			log.Printf("Failed to validate JWT: %v", err)
+			permissionDenied(w)
+			return
+		}
+
+		if !token.Valid {
+			log.Printf("Token is not valid")
+			permissionDenied(w)
+			return
+		}
+
+
+		claims := token.Claims.(jwt.MapClaims)
+		userID := claims["userID"].(string)
+
+		_, err = store.GetUserByID(userID)
+		if err != nil {
+			log.Printf("failed to get user by id: %v", err)
+			permissionDenied(w)
+			return
+		}
+
+		
+		// Token is valid
+		handlerFunc(w, r)
+	}
+}
+
+func CreateJWT(userID int64, secret []byte) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"userID": strconv.Itoa(int(userID)),
+		"exp":    time.Now().Add(time.Hour * 24 * 120).Unix(),
+	})
+
+	tokenString, err := token.SignedString(secret)
+
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func validateJWT(tokenString string) (*jwt.Token, error) {
+	secret := os.Getenv("JWT_SECRET")
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(secret), nil
+	})
+}
+
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
+func permissionDenied(w http.ResponseWriter) {
+	WriteJSON(w, http.StatusUnauthorized, ErrorResponse{
+		Error: fmt.Errorf("permission denied").Error(),
+	})
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.21.6
 
 require github.com/gin-gonic/gin v1.9.1
 
+require github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+
 require (
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
@@ -25,10 +27,10 @@ require (
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
