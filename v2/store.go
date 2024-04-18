fd9f385f0aacd46d420176acf682f3c16cad7060
TimurNiki <timurniki95@gmail.com>
2024-04-18 23:11:15 +0400

    v2-store

--- v2/store.go
+++ v2/store.go
@@ -3,7 +3,15 @@ package main
 import "database/sql"
 
 type Store interface {
-	CreateUser() error
+	CreateUser(u *User) (*User, error)
+	GetUserByID(id string) (*User, error)
+	// Projects
+	CreateProject(p *Project) error
+	GetProject(id string) (*Project, error)
+	DeleteProject(id string) error
+	// Tasks
+	CreateTask(t *Task) (*Task, error)
+	GetTask(id string) (*Task, error)
 }
 
 type Storage struct {
@@ -14,6 +22,34 @@ func NewStore(db *sql.DB) *Storage {
 	return &Storage{db: db}
 }
 
-func (s *Storage) CreateUser() error {
+func (s *Storage) CreateUser(u *User) (*User, error) {
+	return nil
+}
+
+func (s *Storage) GetUserByID(id string) (*User, error) {
+	return nil
+}
+
+func (s *Storage) CreateProject(p *Project) error {
+	return nil
+}
+
+func (s *Storage) GetProject(id string) (*Project, error) {
+	return nil
+}
+
+func (s *Storage) DeleteProject(id string) error {
+	return nil
+}
+
+func (s *Storage) CreateTask(t *Task) (*Task, error) {
+	return nil
+}
+
+func (s *Storage) GetTask(id string) (*Task, error) {
+	return nil
+}
+
+func (s *Storage) DeleteTask(id string) error {
 	return nil
 }
