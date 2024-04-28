b178f795ecde50cb250561b43c5bca580089e4a1
TimurNiki <timurniki95@gmail.com>
2024-04-28 20:43:54 +0400

    v2:projects

--- v2/services/projects/projects.go
+++ v2/services/projects/projects.go
@@ -11,9 +11,56 @@ func (s *ProjectService) RegisterRoutes(r *mux.Router) {
 }
 
 func (s *ProjectService) handleCreateProject(w http.ResponseWriter, r *http.Request) {
+body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusBadRequest)
+		return
+	}
 
+	defer r.Body.Close()
+
+	var project *Project
+	err = json.Unmarshal(body, &project)
+	if err != nil {
+		WriteJSON(w, http.StatusBadRequest, ErrorResponse{Error: "Invalid request payload"})
+		return
+	}
+
+	if project.Name == "" {
+		WriteJSON(w, http.StatusBadRequest, ErrorResponse{Error: "Name is required"})
+		return
+	}
+
+	err = s.store.CreateProject(project)
+	if err != nil {
+		WriteJSON(w, http.StatusInternalServerError, ErrorResponse{Error: "Error creating project"})
+		return
+	}
+
+	WriteJSON(w, http.StatusCreated, project)
 }
 
-func (s *ProjectService) handleGetProject(w http.ResponseWriter, r *http.Request) {}
+func (s *ProjectService) handleGetProject(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
 
-func (s *ProjectService) handleDeleteProject(w http.ResponseWriter, r *http.Request) {}
+	project, err := s.store.GetProject(id)
+	if err != nil {
+		WriteJSON(w, http.StatusInternalServerError, ErrorResponse{Error: "Error getting project"})
+		return
+	}
+
+	WriteJSON(w, http.StatusOK, project)
+}
+
+func (s *ProjectService) handleDeleteProject(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	project,err:= s.store.DeleteProject(	id)
+	if err !=nil{
+		WriteJSON(w, http.StatusInternalServerError, ErrorResponse{Error: "Error deleting project"})
+		return
+	}
+	WriteJSON(w, http.StatusNoContent, nil)
+}
