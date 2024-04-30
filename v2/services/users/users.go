97d08782fb2227a874ddc53ff788059438e10df6
TimurNiki <timurniki95@gmail.com>
2024-04-30 22:46:12 +0400

    users

--- v2/services/users/users.go
+++ v2/services/users/users.go
@@ -1,1 +1,55 @@
 package users
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var errEmailRequired = errors.New("email is required")
+var errFirstNameRequired = errors.New("first name is required")
+var errLastNameRequired = errors.New("last name is required")
+var errPasswordRequired = errors.New("password is required")
+
+
+type UserService struct {
+	store Store
+}
+
+func NewUserService(s Store) *UserService {
+	return &UserService{store: s}
+}
+
+
+func (s *UserService) RegisterRoutes(r *mux.Router) {
+	r.HandleFunc("/users/register", s.handleUserRegister).Methods(http.MethodPost)
+	r.HandleFunc("/users/login", s.handleUserLogin).Methods(http.MethodPost)
+}
+
+func (s *UserService) handleUserRegister(w http.ResponseWriter, r *http.Request){
+
+}
+
+func (s *UserService) handleUserLogin(w http.ResponseWriter, r *http.Request){}
+func validateUserPayload(user *User) error {
+	if user.Email == "" {
+		return errEmailRequired
+	}
+
+	if user.FirstName == "" {
+		return errFirstNameRequired
+	}
+
+	if user.LastName == "" {
+		return errLastNameRequired
+	}
+
+	if user.Password == "" {
+		return errPasswordRequired
+	}
+
+	return nil
+}
