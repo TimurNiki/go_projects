faa0a664b0c6c145a831583b17a937ba4622decb
TimurNiki <timurniki95@gmail.com>
2024-05-09 22:48:59 +0400

    v6: persistance

---
+++ v6/src/infra/middleware/correlation_id.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+)
+
+func AddCorrelationId(app *fiber.App) fiber.Router {
+	return app.Use("/orders", func(ctx *fiber.Ctx) error {
+		correlationId := ctx.Get("x-correlationid")
+
+		if correlationId == "" {
+			return ctx.Status(fiber.StatusBadRequest).JSON("Missing x-correlationid header")
+		}
+
+		_, err := uuid.Parse(correlationId)
+		if err != nil {
+			return ctx.Status(fiber.StatusBadRequest).JSON("Invalid x-correlationid header")
+
+		}
+
+		ctx.Locals("correlationId", correlationId)
+
+		return ctx.Next()
+	})
+}
