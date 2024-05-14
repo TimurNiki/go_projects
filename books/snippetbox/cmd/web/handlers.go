04cce82f9eae35d85371bd4b22b74b053500abfa
TimurNiki <timurniki95@gmail.com>
2024-05-14 23:15:57 +0400

    snippetbox: base+nav tmpl

--- books/snippetbox/cmd/web/handlers.go
+++ books/snippetbox/cmd/web/handlers.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -14,44 +13,48 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
+
+
+	files := []string{
+		"./ui/html/base.tmpl",
+		"./ui/html/pages/home.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		}
+	   
 	// Use the template.ParseFiles() function to read the template file into a
- // template set. If there's an error, we log the detailed error message and use
- // the http.Error() function to send a generic 500 Internal Server Error
- // response to the user.
-
- ts,err:=template.ParseFiles("./ui/html/pages/home.html")
- if err!=nil{
-	log.Print(err.Error())
-	http.Error(w,"Internal Server Error",500)
-	return
- }
-// We then use the Execute() method on the template set to write the
- // template content as the response body. The last parameter to Execute()
- // represents any dynamic data that we want to pass in, which for now we'll
- // leave as nil.
-
- err = ts.Execute(w,nil)
- if err!=nil{
-	log.Print(err.Error())
-	http.Error(w,"Internal Server Error",500)
- }
-
+	// template set. If there's an error, we log the detailed error message and use
+	// the http.Error() function to send a generic 500 Internal Server Error
+	// response to the user.
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	// We then use the Execute() method on the template set to write the
+	// template content as the response body. The last parameter to Execute()
+	// represents any dynamic data that we want to pass in, which for now we'll
+	// leave as nil.
+	err = ts.ExecuteTemplate(w,"base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-	http.NotFound(w, r)
-	return
+		http.NotFound(w, r)
+		return
 	}
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
-   }
-   func snippetCreate(w http.ResponseWriter, r *http.Request) {
+}
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-	w.Header().Set("Allow", http.MethodPost)
-	http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-	return
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
 	}
 	w.Write([]byte("Create a new snippet..."))
-   }
-   
+}
