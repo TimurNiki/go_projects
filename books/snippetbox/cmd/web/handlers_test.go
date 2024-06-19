3d26ce5ca5e6b09efeef2035c414c1e9a1fc9e4f
TimurNiki <timurniki95@gmail.com>
2024-06-19 23:30:49 +0400

    snipbox: TestSnippetCreate

--- books/snippetbox/cmd/web/handlers_test.go
+++ books/snippetbox/cmd/web/handlers_test.go
@@ -5,8 +5,11 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
-	"snippetbox.alexedwards.net/internal/assert"
+	"net/url"
 	"testing"
+
+	"github.com/TimurNiki/go_api_tutorial/books/snippetbox/internal/assert"
+	"snippetbox.alexedwards.net/internal/assert"
 )
 
 func TestPing(t *testing.T) {
@@ -156,9 +159,13 @@ func TestUserSignup(t *testing.T) {
 }
 
 func TestUserSignup(t *testing.T) {
+	// Create the application struct containing our mocked dependencies and set
+	// up the test server for running an end-to-end test
 	app := newTestApplication(t)
 	ts := newTestServer(t, app.routes())
 	defer ts.Close()
+	// Make a GET /user/signup request and then extract the CSRF token from the
+	// response body.
 	_, _, body := ts.get(t, "/user/signup")
 	validCSRFToken := extractCSRFToken(t, body)
 	const (
@@ -260,4 +267,37 @@ func TestUserSignup(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestSnippetCreate(t *testing.T) {
+	// Create the application struct containing our mocked dependencies and set
+	// up the test server for running an end-to-end test.
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	t.Run("Unauthenticated", func(t *testing.T) {
+		code, headers, _ := ts.get(t, "/snippet/create)")
+		assert.Equal(t, code, http.StatusSeeOther)
+		assert.Equal(t, headers.Get("Location"), "/user/login")
+	})
+
+	t.Run("Authenticated", func(t *testing.T) {
+		// Make a GET /user/login request and extract the CSRF token from the
+		// response.
+		_, _, body := ts.get(t, "/user/login")
+		csrfToken := extractCSRFToken(t, body)
+		// Make a POST /user/login request using the extracted CSRF token and
+		// credentials from our the mock user model.
+		form := url.Values{}
+		form.Add("email", "alice@example.com")
+		form.Add("password", "pa$$word")
+		form.Add("csrf_token", csrfToken)
+		ts.postForm(t, "/user/login", form)
+		// Then check that the authenticated user is shown the create snippet
+		// form.
+		code, _, body := ts.get(t, "/snippet/create")
+		assert.Equal(t, code, http.StatusOK)
+		assert.StringContains(t, body, "<form action='/snippet/create' method='POST'>")
+	})
 }
