a40ff4668d6e414e0e09a2bacef25121c33a9ad1
tniki <timurniki95@gmail.com>
2024-09-21 22:06:04 +0400

    v5-store

---
+++ v5/internal/store/storage.go
@@ -0,0 +1,30 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"time"
+)
+
+var (
+	ErrNotFound          = errors.New("resource not found")
+	ErrConflict          = errors.New("resource already exists")
+	QueryTimeoutDuration = time.Second * 5
+)
+
+type Storage struct {
+	Posts interface {
+		GetByID(context.Context, int64) (*Post, error)
+		Create(context.Context, *Post) error
+		Delete(context.Context, int64) error
+		Update(context.Context, *Post) error
+		GetUserFeed(context.Context, int64, PaginatedFeedQuery) ([]PostWithMetadata, error)
+	}
+}
+
+func NewStorage(db *sql.DB) Storage {
+	return Storage{
+		Posts: &PostStore{db},
+	}
+}
