b1f8531125606db13eaccadd5f99ae45887b1436
tniki <timurniki95@gmail.com>
2024-09-23 22:21:16 +0400

    v5:create+rolename

--- v5/internal/store/users.go
+++ v5/internal/store/users.go
@@ -1,8 +1,10 @@
 package store
 
 import (
+	"context"
 	"database/sql"
 	"errors"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var (
@@ -26,6 +28,100 @@ type password struct {
 	hash []byte
 }
 
+func (p *password) Set(text string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(text), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	p.text = &text
+	p.hash = hash
+
+	return nil
+}
+
 type UserStore struct {
 	db *sql.DB
+}
+
+func (s *UserStore) Create(ctx context.Context, tx *sql.Tx, user *User) error {
+	query := `
+		INSERT INTO users (username, password, email, role_id) VALUES 
+    ($1, $2, $3, (SELECT id FROM roles WHERE name = $4))
+    RETURNING id, created_at
+	`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
+	role := user.Role.Name
+	if role == "" {
+		role = "user"
+	}
+
+	err := tx.QueryRowContext(
+		ctx,
+		query,
+		user.Username,
+		user.Password.hash,
+		user.Email,
+		role,
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+			return ErrDuplicateEmail
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_username_key"`:
+			return ErrDuplicateUsername
+		default:
+			return err
+
+		}
+
+	}
+	return nil
+}
+
+func (s *UserStore) GetByID(ctx context.Context, userID int64) (*User, error){
+	query := `
+		SELECT users.id, username, email, password, created_at, roles.*
+		FROM users
+		JOIN roles ON (users.role_id = roles.id)
+		WHERE users.id = $1 AND is_active = true
+	`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
+	role := user.Role.Name
+	if role == "" {
+		role = "user"
+	}
+	
+	err := tx.QueryRowContext(
+		ctx,
+		query,
+		user.Username,
+		user.Password.hash,
+		user.Email,
+		role,
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		switch {
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+			return ErrDuplicateEmail
+		case err.Error() == `pq: duplicate key value violates unique constraint "users_username_key"`:
+			return ErrDuplicateUsername
+		default:
+			return err
+		}
+	}
+
+	return nil
 }
