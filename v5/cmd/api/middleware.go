4192cb6b5945043a9f2bef45fa765d3b2c62ae40
tniki <timurniki95@gmail.com>
2024-10-11 23:27:50 +0400

    v5:middleware

--- v5/cmd/api/middleware.go
+++ v5/cmd/api/middleware.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"context"
+	"net/http"
 	"v5/internal/store"
 )
 
+
+func(app *application) checkRolePrecedence(ctx context.Context, user *store.User, roleName string) (bool, error)  {
+	role,err:=app.store.Roles.GetByName(ctx, roleName)
+	if err != nil {
+		return false, err
+	}
+
+	return user.Role.Level >= role.Level, nil
+}
 func (app *application) getUser(ctx context.Context, userID int64) (*store.User, error) {
 	if !app.config.redisCfg.enabled {
 		return app.store.Users.GetByID(ctx, userID)
@@ -26,4 +36,17 @@ func (app *application) getUser(ctx context.Context, userID int64) (*store.User,
 	}
 
 	return user, nil
+}
+
+func (app *application) RateLimiterMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if app.config.rateLimiter.Enabled {
+			if allow, retryAfter := app.rateLimiter.Allow(r.RemoteAddr); !allow {
+				app.rateLimitExceededResponse(w, r, retryAfter.String())
+				return
+			}
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
